4e4594d212337136528febdab77478d6e7d54794
Dryw Wade <dryw.wade@sparkfun.com>
2022-04-21 13:59:38 -0600

    Update basic readings examples
    Both I2C and SPI work correctly

--- examples/Example1_BasicReadingsI2C/Example1_BasicReadingsI2C.ino
+++ examples/Example1_BasicReadingsI2C/Example1_BasicReadingsI2C.ino
@@ -1,23 +1,29 @@
 #include <Wire.h>
 #include "SparkFunBMP384.h"
 
+// Create a new sensor object
 BMP384 pressureSensor;
+
+// I2C address selection
+uint8_t i2cAddress = BMP384_I2C_ADDRESS_DEFAULT; // 0x77
+//uint8_t i2cAddress = BMP384_I2C_ADDRESS_SECONDARY; // 0x76
 
 void setup()
 {
+    // Start serial
     Serial.begin(115200);
     Serial.println("BMP384 example begin!");
 
+    // Initialize the I2C library
     Wire.begin();
 
-    // Uncomment to change I2C address from default
-    //pressureSensor.setI2CAddress(BMP384_I2C_ADDRESS_0);
-
-    // Check if sensor is connected
-    while(!pressureSensor.beginI2C())
+    // Check if sensor is connected and initialize
+    // Address is optional (defaults to 0x77)
+    while(pressureSensor.beginI2C(i2cAddress) != BMP3_OK)
     {
         // Not connected, inform user
         Serial.println("Error: BMP384 not connected, check wiring and I2C address!");
+        Serial.println(i2cAddress);
 
         // Wait a bit to see if connection is established
         delay(1000);
@@ -28,14 +34,18 @@ void setup()
 
 void loop()
 {
-    // Print temperature and pressure every second
+    // Get measurements from the sensor
+    bmp3_data data = pressureSensor.getSensorData();
+
+    // Print temperature and pressure
     Serial.print("Temperature (C): ");
-    Serial.print(pressureSensor.getTemperature());
+    Serial.print(data.temperature);
     
     Serial.print("\t\t");
 
     Serial.print("Pressure (Pa): ");
-    Serial.println(pressureSensor.getPressure());
+    Serial.println(data.pressure);
 
+    // Only print every second
     delay(1000);
 }
--- examples/Example2_BasicReadingsSPI/Example2_BasicReadingsSPI.ino
+++ examples/Example2_BasicReadingsSPI/Example2_BasicReadingsSPI.ino
@@ -1,22 +1,28 @@
 #include <SPI.h>
 #include "SparkFunBMP384.h"
 
+// Create a new sensor object
 BMP384 pressureSensor;
 
+// SPI parameters
 uint8_t csPin = 10;
+uint32_t clockFrequency = 100000;
 
 void setup()
 {
+    // Start serial
     Serial.begin(115200);
     Serial.println("BMP384 example begin!");
 
+    // Initialize the SPI library
     SPI.begin();
 
-    // Check if sensor is connected
-    while(!pressureSensor.beginSPI(csPin))
+    // Check if sensor is connected and initialize
+    // Clock frequency is optional (defaults to 100kHz)
+    while(pressureSensor.beginSPI(csPin, clockFrequency) != BMP3_OK)
     {
         // Not connected, inform user
-        Serial.println("Error: BMP384 not connected, check wiring!");
+        Serial.println("Error: BMP384 not connected, check wiring and CS pin!");
 
         // Wait a bit to see if connection is established
         delay(1000);
@@ -27,14 +33,18 @@ void setup()
 
 void loop()
 {
-    // Print temperature and pressure every second
+    // Get measurements from the sensor
+    bmp3_data data = pressureSensor.getSensorData();
+
+    // Print temperature and pressure
     Serial.print("Temperature (C): ");
-    Serial.print(pressureSensor.getTemperature());
+    Serial.print(data.temperature);
     
     Serial.print("\t\t");
 
     Serial.print("Pressure (Pa): ");
-    Serial.println(pressureSensor.getPressure());
+    Serial.println(data.pressure);
 
+    // Only print every second
     delay(1000);
 }
