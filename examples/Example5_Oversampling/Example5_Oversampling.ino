cda68813374a661391cf66840aea295b3d14d416
Dryw Wade <dryw.wade@sparkfun.com>
2022-04-21 17:51:25 -0600

    Update oversampling example

--- examples/Example5_Oversampling/Example5_Oversampling.ino
+++ examples/Example5_Oversampling/Example5_Oversampling.ino
@@ -1,20 +1,25 @@
 #include <Wire.h>
 #include "SparkFunBMP384.h"
 
+// Create a new sensor object
 BMP384 pressureSensor;
+
+// I2C address selection
+uint8_t i2cAddress = BMP384_I2C_ADDRESS_DEFAULT; // 0x77
+//uint8_t i2cAddress = BMP384_I2C_ADDRESS_SECONDARY; // 0x76
 
 void setup()
 {
+    // Start serial
     Serial.begin(115200);
     Serial.println("BMP384 example begin!");
 
+    // Initialize the I2C library
     Wire.begin();
 
-    // Uncomment to change I2C address from default
-    //pressureSensor.setI2CAddress(BMP384_I2C_ADDRESS_0);
-
-    // Check if sensor is connected
-    while(!pressureSensor.beginI2C())
+    // Check if sensor is connected and initialize
+    // Address is optional (defaults to 0x77)
+    while(pressureSensor.beginI2C(i2cAddress) != BMP3_OK)
     {
         // Not connected, inform user
         Serial.println("Error: BMP384 not connected, check wiring and I2C address!");
@@ -24,32 +29,59 @@ void setup()
     }
 
     Serial.println("BMP384 connected!");
+    
+    // Variable to track errors returned by API calls
+    int8_t err = BMP3_OK;
 
-    // The oversampling (OSR) multipliers should be a power of 2, with a maximum of 32 (2^5).
+    // Both the temperature and pressure sensors support oversampling, where
+    // multiple measurements are performed to increase resolution and decrease
+    // noise. By default, no oversampling is used.
     // At 1x OSR, we get 16-bit resolution from each sensor (0.005C and 2.64Pa).
     // At 32x OSR, we get 21-bit resolution from each sensor (0.00015C and 0.085Pa).
-    pressureSensor.setTemperatureOSRMultiplier(1);
-    pressureSensor.setPressureOSRMultiplier(1);
+    bmp3_odr_filter_settings osrMultipliers =
+    {
+        .press_os = BMP3_OVERSAMPLING_32X,
+        .temp_os = BMP3_OVERSAMPLING_2X
+    };
+    err = pressureSensor.setOSRMultipliers(osrMultipliers);
+    if(err)
+    {
+        // Setting ODR failed, most likely an invalid multiplier (code -3)
+        Serial.print("Error setting OSR! Error code: ");
+        Serial.println(err);
+    }
 
-    /////////////////////////////////////////////////////////////////
-    //                           WARNING                           //
-    //                                                             //
-    // As of this time, setting any value higher than 1 causes     //
-    // sensor values to stop changing. The cause and solution are  //
-    // not yet known, this is being worked on!                     //
-    /////////////////////////////////////////////////////////////////
+    // Note - increasing oversampling also requires ODR to be increased.
+    // setOSRMultipliers() will automatically increase the ODR if needed, but
+    // will still return BMP3_OK. You can check whether it was changed by
+    // calling getODRFrequency()
 }
 
 void loop()
 {
-    // Print temperature and pressure every second
-    Serial.print("Temperature (C): ");
-    Serial.print(pressureSensor.getTemperature());
-    
-    Serial.print("\t\t");
+    // Get measurements from the sensor
+    bmp3_data data = {0};
+    int8_t err = pressureSensor.getSensorData(&data);
 
-    Serial.print("Pressure (Pa): ");
-    Serial.println(pressureSensor.getPressure());
+    // Check whether data was acquired successfully
+    if(err == BMP3_OK)
+    {
+        // Acquisistion succeeded, print temperature and pressure
+        Serial.print("Temperature (C): ");
+        Serial.print(data.temperature);
 
+        Serial.print("\t\t");
+
+        Serial.print("Pressure (Pa): ");
+        Serial.println(data.pressure);
+    }
+    else
+    {
+        // Acquisition failed, most likely a communication error (code -2)
+        Serial.print("Error getting data from sensor! Error code: ");
+        Serial.println(err);
+    }
+
+    // Only print every second
     delay(1000);
 }
