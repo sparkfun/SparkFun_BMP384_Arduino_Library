c1a1336602dcdb1f45f228810956b1257b11215e
Dryw Wade <dryw.wade@sparkfun.com>
2022-04-21 15:35:39 -0600

    Update interrupt example

--- examples/Example3_Interrupts/Example3_Interrupts.ino
+++ examples/Example3_Interrupts/Example3_Interrupts.ino
@@ -1,27 +1,35 @@
 #include <Wire.h>
 #include "SparkFunBMP384.h"
 
+// Create a new sensor object
 BMP384 pressureSensor;
 
+// I2C address selection
+uint8_t i2cAddress = BMP384_I2C_ADDRESS_DEFAULT; // 0x77
+//uint8_t i2cAddress = BMP384_I2C_ADDRESS_SECONDARY; // 0x76
+
+// Pin used for interrupt detection
 int interruptPin = 2;
 
+// Flag to know when interrupts occur
 volatile bool interruptOccurred = false;
 
 void setup()
 {
+    // Start serial
     Serial.begin(115200);
     Serial.println("BMP384 example begin!");
 
+    // Initialize the I2C library
     Wire.begin();
 
-    // Uncomment to change I2C address from default
-    //pressureSensor.setI2CAddress(BMP384_I2C_ADDRESS_0);
-
-    // Check if sensor is connected
-    while(!pressureSensor.beginI2C())
+    // Check if sensor is connected and initialize
+    // Address is optional (defaults to 0x77)
+    while(pressureSensor.beginI2C(i2cAddress) != BMP3_OK)
     {
         // Not connected, inform user
         Serial.println("Error: BMP384 not connected, check wiring and I2C address!");
+        Serial.println(i2cAddress);
 
         // Wait a bit to see if connection is established
         delay(1000);
@@ -29,26 +37,35 @@ void setup()
 
     Serial.println("BMP384 connected!");
 
-    // By default, the BMP384 samples every 5ms. We can set the ODR prescaler
-    // to slow down the output data rate. The prescaler should be a power of 2,
-    // with a maximum of 2^17. We can use a prescaler of 256 to get measurements
-    // every 1.28 seconds
-    pressureSensor.setODRPrescaler(256);
+    // Variable to track errors returned by API calls
+    int8_t err = BMP3_OK;
+
+    // By default, the BMP384 samples at 200Hz. However we don't want interrupts to
+    // trigger that fast in this example, so we can lower the output data rate (ODR)
+    // with this function. In this case, we're setting it to 1.5Hz
+    err = pressureSensor.setODRFrequency(BMP3_ODR_1_5_HZ);
+    if(err != BMP3_OK)
+    {
+        // Setting ODR failed, most likely an invalid frequncy (code -3)
+        Serial.print("ODR setting failed! Error code: ");
+        Serial.println(err);
+    }
 
     // Configure the BMP384 to trigger interrupts whenever a measurement is performed
-    BMP384_InterruptConfig config =
+    bmp3_int_ctrl_settings interruptSettings =
     {
-        .flags =
-        {
-            .openDrain     = false, // Open-drain or push/pull
-            .activeHigh    = true,  // Active high or low signal level
-            .latch         = false, // Prevent interrupt from automatically clearing
-            .fifoWatermark = false, // Trigger interrupt when FIFO watermark is reached
-            .fifoFull      = false, // Trigger interrupt when FIFO is full
-            .dataReady     = true   // Trigger interrupt when measurement is complete
-        }
+        .output_mode = BMP3_INT_PIN_PUSH_PULL,
+        .level       = BMP3_INT_PIN_ACTIVE_HIGH,
+        .latch       = BMP3_INT_PIN_NON_LATCH,
+        .drdy_en     = true
     };
-    pressureSensor.setInterruptConfig(config);
+    err = pressureSensor.setInterruptSettings(interruptSettings);
+    if(err != BMP3_OK)
+    {
+        // Interrupt settings failed, most likely a communication error (code -2)
+        Serial.print("Interrupt settings failed! Error code: ");
+        Serial.println(err);
+    }
 
     // Setup interrupt handler
     attachInterrupt(digitalPinToInterrupt(interruptPin), bmp384InterruptHandler, RISING);
@@ -59,22 +76,55 @@ void loop()
     // Wait for interrupt to occur
     if(interruptOccurred)
     {
-        // Make sure this is the "data ready" interrupt condition
-        BMP384_InterruptStatus status = pressureSensor.getInterruptStatus();
-        if(status.flags.dataReady)
-        {
-            // Print the current temperature and pressure
-            Serial.print("Temperature (C): ");
-            Serial.print(pressureSensor.getTemperature());
-            
-            Serial.print("\t\t");
-
-            Serial.print("Pressure (Pa): ");
-            Serial.println(pressureSensor.getPressure());
-        }
-
         // Reset flag for next interrupt
         interruptOccurred = false;
+
+        Serial.print("Interrupt occurred!\t\t");
+
+        // Variable to track errors returned by API calls
+        int8_t err = BMP3_OK;
+
+        // Get the interrupt status to know which condition triggered
+        bmp3_int_status interruptStatus = {0};
+        err = pressureSensor.getInterruptStatus(&interruptStatus);
+        if(err != BMP3_OK)
+        {
+            // Acquisition failed, most likely a communication error (code -2)
+            Serial.print("Get interrupt status failed! Error code: ");
+            Serial.println(err);
+            return;
+        }
+
+        // Make sure this is the "data ready" interrupt condition
+        if(interruptStatus.drdy)
+        {
+            // Get measurements from the sensor
+            bmp3_data data = {0};
+            int8_t err = pressureSensor.getSensorData(&data);
+
+            // Check whether data was acquired successfully
+            if(err == BMP3_OK)
+            {
+                // Acquisistion succeeded, print temperature and pressure
+                Serial.print("Temperature (C): ");
+                Serial.print(data.temperature);
+
+                Serial.print("\t\t");
+
+                Serial.print("Pressure (Pa): ");
+                Serial.println(data.pressure);
+            }
+            else
+            {
+                // Acquisition failed, most likely a communication error (code -2)
+                Serial.print("Error getting data from sensor! Error code: ");
+                Serial.println(err);
+            }
+        }
+        else
+        {
+            Serial.println("Wrong interrupt condition!");
+        }
     }
 }
 
